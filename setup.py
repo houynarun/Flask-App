f3724cd0496fb787734e01d77f51663b32fc5112
Narun <qa@morakot.it>
2020-07-16 10:14:59 +0700

    update setup.py file

--- setup.py
+++ setup.py
@@ -2,34 +2,23 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mktpip',
+    name='flask-app',
     version='0.0.1',
-    description="mktpip is command-line utility for install python private repository.",
+    description="flask app sample python package for developer learning.",
     long_description = 'long_description',
-    url="https://www.morakot.it",
-    author='Rim Sovankiry',
-    author_email="sovankiry@morakot.it",
+    url="https://flask.palletsprojects.com/en/1.1.x/tutorial/",
+    author='Houy Narun',
+    author_email="houy.narun@gmail.com",
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     zip_safe=False,
     data_files=[
         ('',['favo.png','NEWS.rst'])
     ],
-    project_urls ={
-           'Documentation': 'http://research-development.gitlab-pages.morakot.it/docs/vb-docs',
-           'Source Code': 'https://git.morakot.it/toplevel/mktpip',
-           'Bug Tracker': 'https://support.morakot.it/projects/bug?jump=welcome',
-           'Feature Suggestion': ['fa fa-question','https://support.morakot.it/projects/new-feature/roadmap'],
-           'Realease Note':['fa fa-bullhorn','https://git.morakot.it/toplevel/mktpip/blob/dev/NEWS.rst'],
-    },
     # install_requires=Requirement,
-    license="Morakot Technology",
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 2 :: Only",
         "Operating System :: OS Independent",
-    ],
-    entry_points='''
-        [console_scripts]
-        mktpip=mktpip.scripts.cli:cli
-    ''',
+    ]
 )
